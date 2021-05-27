530013815e034b723c86b71f1c0bd7db4cb64a87
firaga <zhou.jichen@gmail.com>
2021-05-27 14:32:40 +0800

    1.修复第三层循环判断边界 2. 增加去重判断 3. third值总是从最后开始

--- threeSum/three_sum.go
+++ threeSum/three_sum.go
@@ -8,20 +8,33 @@ func threeSum(nums []int) [][]int {
 	ans := make([][]int, 0)
 	l := len(nums)
 	var first, second, third int
-	third = l - 1
+
 	//快排
 	sort.Ints(nums)
 
 	for first = 0; first < l; first++ {
+		if first > 0 && nums[first] == nums[first-1] {
+			continue
+		}
 		reversFirstValue := -nums[first]
-		for second = first + 1; second < l && nums[second] != nums[second-1]; second++ {
-			for third > second {
+		for second = first + 1; second < l; second++ {
+			if second > first+1 && nums[second] == nums[second-1] {
+				continue
+			}
+			for third = l - 1; third > second; {
+				if third < l-1 && nums[third] == nums[third+1] {
+					third--
+					continue
+				}
 				if nums[third]+nums[second] > reversFirstValue {
+					//second+third > -first
 					third--
 				} else if nums[third]+nums[second] == reversFirstValue {
+					//second+third = -first
 					ans = append(ans, []int{nums[first], nums[second], nums[third]})
-					break
+					third--
 				} else {
+					//second+third < -first
 					second++
 				}
 			}
--- threeSum/three_sum_test.go
+++ threeSum/three_sum_test.go
@@ -6,7 +6,14 @@ import (
 )
 
 func TestThreeSum(t *testing.T) {
-	nums := []int{-1, -1, 0, 1, 2. - 1, -4}
+	//nums := []int{-1, -1, 0, 1, 2. - 1, -4}
+	//nums := []int{-1, 0, 1, 2, -1, -4, 2}
+	//-4,-1,-1,-1,0,1,2
+	//nums := []int{0, 0, 0, 0}
+	// -4,-3,-2,-1,-1,0,0,1,2,3,4
+	nums := []int{-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4}
 	res := threeSum(nums)
+	fmt.Println(res)
+	res = officialThreeSum(nums)
 	fmt.Println(res)
 }
