7d152fe9fe78544a7e2d1cbb49b404611f45f367
firaga <zhou.jichen@gmail.com>
2021-05-27 11:43:21 +0800

    threeSum algorithm

--- threeSum/three_sum.go
+++ threeSum/three_sum.go
@@ -6,17 +6,24 @@ import (
 
 func threeSum(nums []int) [][]int {
 	ans := make([][]int, 0)
-	l := len(ans)
+	l := len(nums)
 	var first, second, third int
 	third = l - 1
 	//快排
 	sort.Ints(nums)
 
 	for first = 0; first < l; first++ {
-		reversFirst := -first
-		for second = first + 1; second < l; second++ {
-			for ; third > second; third-- {
-
+		reversFirstValue := -nums[first]
+		for second = first + 1; second < l && nums[second] != nums[second-1]; second++ {
+			for third > second {
+				if nums[third]+nums[second] > reversFirstValue {
+					third--
+				} else if nums[third]+nums[second] == reversFirstValue {
+					ans = append(ans, []int{nums[first], nums[second], nums[third]})
+					break
+				} else {
+					second++
+				}
 			}
 		}
 	}
--- threeSum/three_sum_test.go
+++ threeSum/three_sum_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestThreeSum(t *testing.T) {
-	nums := []int{-1, 0, 1, 2. - 1, -4}
+	nums := []int{-1, -1, 0, 1, 2. - 1, -4}
 	res := threeSum(nums)
 	fmt.Println(res)
 }
