c0cea158cc9d054b04b0edd746e6df0d4bf81e4c
firaga <zhou.jichen@gmail.com>
2021-09-28 19:09:12 +0800

    另一种快速排序

--- quickSort/quick_sort.go
+++ quickSort/quick_sort.go
@@ -23,3 +23,31 @@ func quickSort(in []int) {
 	quickSort(in[:minPrt-1])
 	quickSort(in[minPrt:])
 }
+
+func quickSort2(sortArray []int, left, right int) {
+	if left < right {
+		key := sortArray[(left+right)/2]
+		i := left
+		j := right
+
+		for {
+			for sortArray[i] < key {
+				i++
+			}
+			for sortArray[j] > key {
+				j--
+			}
+			if i >= j {
+				break
+			}
+			sortArray[i], sortArray[j] = sortArray[j], sortArray[i]
+		}
+
+		quickSort2(sortArray, left, i-1)
+		quickSort2(sortArray, j+1, right)
+	}
+}
+
+func QuickSort2(values []int) {
+	quickSort2(values, 0, len(values)-1)
+}
