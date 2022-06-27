7e492e84dc1b4e8af4fb42da4210193faf26aa9e
firaga <zhou.jichen@gmail.com>
2022-06-27 11:03:15 +0800

    快排算法

--- quickSort/quick_sort.go
+++ quickSort/quick_sort.go
@@ -1,31 +1,33 @@
 package quickSort
 
-import "fmt"
+func quickSort(inputArray []int) {
 
-func quickSort(in []int) {
-
-	//fmt.Println(in)
-	pivot := len(in) - 1
+	//fmt.Println(inputArray)
+	pivot := len(inputArray) - 1
+	//遍历指针, 交换位置指针, 指向小于pivot的元素后一个位置
 	var traversalPtr, minPrt int
 	for ; traversalPtr <= pivot; traversalPtr++ {
-		if in[traversalPtr] <= in[pivot] {
+		if inputArray[traversalPtr] <= inputArray[pivot] {
 			//swap
-			in[minPrt], in[traversalPtr] = in[traversalPtr], in[minPrt]
+			inputArray[minPrt], inputArray[traversalPtr] = inputArray[traversalPtr], inputArray[minPrt]
 			//递增,指向小于in[pivot]的下一个元素
 			minPrt++
 		}
 	}
-	if len(in) <= 2 {
+	//直接退出
+	if len(inputArray) <= 2 {
 		return
 	}
-	fmt.Println("minPtr", minPrt)
+	//fmt.Println("minPtr", minPrt)
 	//if minPrt-2 >= 0 {
-	quickSort(in[:minPrt-1])
-	quickSort(in[minPrt:])
+	quickSort(inputArray[:minPrt-1])
+	quickSort(inputArray[minPrt:])
 }
 
+//todo 处理重复有问题
 func quickSort2(sortArray []int, left, right int) {
 	if left < right {
+		//选定pivot,位置随意
 		key := sortArray[(left+right)/2]
 		i := left
 		j := right
@@ -50,4 +52,34 @@ func quickSort2(sortArray []int, left, right int) {
 
 func QuickSort2(values []int) {
 	quickSort2(values, 0, len(values)-1)
+}
+
+//todo test
+func QuickSort3(arr []int, left, right int) {
+	if left >= right {
+		return
+	}
+	l := left
+	r := right
+	base := arr[left]
+	for l < r {
+		for l < r && arr[r] >= base {
+			r--
+		}
+		if l < r {
+			arr[l] = arr[r]
+		}
+
+		for l < r && arr[l] <= base {
+			l++
+		}
+
+		if l < r {
+			arr[r] = arr[l]
+			r++
+		}
+	}
+	arr[l] = base
+	QuickSort3(arr, left, l-1)
+	QuickSort3(arr, l+1, right)
 }
--- quickSort/quick_sort_test.go
+++ quickSort/quick_sort_test.go
@@ -9,8 +9,14 @@ func TestQuickSort(t *testing.T) {
 	//in := []int{1, 4, 3, 9, 3}
 	//in := []int{3, 1, 4, 100}
 	//in := []int{3, 4, 2, 9}
-	in := []int{-1, -1, 3, 4, 2, 0, 9}
-	//in := []int{9, 5, 4, 3, 1}
-	quickSort(in)
-	fmt.Println(in)
+	//in := []int{-1, -1, 3, 4, 2, 0, 9}
+	//in2 := []int{-1,  3, 4, 2, 0, 9}
+	//in2 := []int{3, 4, 2, 9}
+	//quickSort(in)
+	//fmt.Println(in)
+	//QuickSort2(in2)
+	//fmt.Println(in2)
+	in3 := []int{-1, 3, 4, 2, 0, 9}
+	QuickSort3(in3, 0, len(in3)-1)
+	fmt.Println(in3)
 }
