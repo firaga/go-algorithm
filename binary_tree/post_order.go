2de58c17f437c3538e98ed34c2b4f540bb3a60b5
firaga <zhou.jichen@gmail.com>
2022-09-02 15:59:37 +0800

    后序遍历

--- binary_tree/post_order.go
+++ binary_tree/post_order.go
@@ -13,3 +13,29 @@ func PostOrder(node *Node) {
 	PostOrder(node.right)
 	fmt.Print(strconv.Itoa(node.value) + " ")
 }
+func PostOrderNonRecursive(node *Node) {
+	s := NewStack()
+	lastPop := node
+	cur := node
+	for {
+		if cur == nil && s.Len() == 0 {
+			break
+		}
+		for {
+			if cur == nil {
+				break
+			}
+			s.Push(cur)
+			cur = cur.left
+		}
+		cur = s.Peek().(*Node)
+		if cur.right == nil || cur.right == lastPop {
+			_ = s.Pop().(*Node)
+			fmt.Print(strconv.Itoa(cur.value) + " ")
+			lastPop = cur
+			cur = nil
+		} else {
+			cur = cur.right
+		}
+	}
+}
--- binary_tree/post_order_test.go
+++ binary_tree/post_order_test.go
@@ -7,3 +7,9 @@ func TestPostOrder(t *testing.T) {
 	root := LevelOrderBuild(input)
 	PostOrder(root)
 }
+
+func TestPostOrderNonRecursive(t *testing.T) {
+	input := []int{1, 2, 3, 4, 5, 6, 7}
+	root := LevelOrderBuild(input)
+	PostOrderNonRecursive(root)
+}
