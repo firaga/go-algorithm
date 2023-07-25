2539039f7a610b66065c69e0f94e4288e2db0c6f
firaga <zhou.jichen@gmail.com>
2023-07-25 17:07:00 +0800

    面试题： 反转字符串

--- interview/string_reverse_test.go
+++ interview/string_reverse_test.go
@@ -6,30 +6,42 @@ import (
 )
 
 func TestReverseString(t *testing.T) {
-	s := "the####code"
-	o := reverseStr(s)
+	//s := "  It the  "
+	s := "  hello world  "
+	o := reverseWords(s)
 	fmt.Println(o)
 }
 
 // 反转字符串
-func reverseStr(s string) (output string) {
+func reverseWords(s string) string {
+	output := ""
 	l := len(s) - 1
 	r := l
 	for {
 		if l >= 0 {
-			for l >= 0 && s[l] == '#' {
+			for l >= 0 && s[l] == ' ' {
 				l--
 				r = l
 			}
-			for l >= 0 && s[l] != '#' {
+			for l >= 0 && s[l] != ' ' {
 				l--
 			}
-			output += s[l+1 : r+1]
-			output = output + " "
+			//注意边界判断条件
+			//l未走到头的处理方法
+			if l != -1 {
+				output += s[l+1 : r+1]
+				output = output + " "
+			} else { //l走到头（==-1）的处理方法
+				if r >= 0 {
+					output += s[0 : r+1]
+					output = output + " "
+				}
+			}
 		} else {
+			//最后截断
 			output = output[:len(output)-1]
 			break
 		}
 	}
-	return
+	return output
 }
