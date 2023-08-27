25fb17b1d9f4a4a1966af6f01f85714b1f09889f
firaga <zhou.jichen@gmail.com>
2023-08-27 22:20:54 +0800

    topK

--- quickSort/heap_sort_test.go
+++ quickSort/heap_sort_test.go
@@ -5,6 +5,10 @@ import (
 	"testing"
 )
 
+// 参考1 数据结构与算法之美 - 28堆和堆排序
+//	自上而下建堆，对非叶子节点heapify
+// 参考2 https://blog.csdn.net/qq_37874758/article/details/120758589
+// 自下而上建堆，
 func TestHeapSort(t *testing.T) {
 	arr := []int{-1, 3, 4, 2, 0, 9}
 	//arr := []int{3, 2, 8, 5, 9}
