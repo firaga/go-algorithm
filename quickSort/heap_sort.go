19cda7efcf11570bb41dba5d0f3d34ac279149d4
firaga <zhou.jichen@gmail.com>
2023-08-27 20:20:45 +0800

    堆排序

---
+++ quickSort/heap_sort.go
@@ -0,0 +1,44 @@
+package quickSort
+
+func headSort(arr []int) {
+	buildHeap(arr, len(arr))
+	heapSize := len(arr)
+	for heapSize > 1 { //heapSize ==1 时不需要继续执行
+		swap(arr, 0, heapSize-1)
+		heapSize--
+		heapify(arr, heapSize, 0)
+	}
+}
+
+func buildHeap(arr []int, len int) {
+	lastNoLeafNodeIndex := len/2 - 1
+	for i := lastNoLeafNodeIndex; i >= 0; i-- {
+		heapify(arr, len, i)
+	}
+}
+
+//n length of arr
+///i  root of a tree or subtree, it's a index of arr
+func heapify(arr []int, n, i int) {
+	for true {
+		maxPos := i
+		//判断左子节点
+		if i*2+1 < n && arr[maxPos] < arr[i*2+1] {
+			maxPos = i*2 + 1
+		}
+		//判断右子节点
+		if i*2+2 < n && arr[maxPos] < arr[i*2+2] {
+			maxPos = i*2 + 2
+		}
+		if maxPos == i {
+			break
+		}
+		swap(arr, i, maxPos)
+		//开启下一轮交换
+		i = maxPos
+	}
+}
+
+func swap(arr []int, a int, b int) {
+	arr[a], arr[b] = arr[b], arr[a]
+}
---
+++ quickSort/heap_sort_test.go
@@ -0,0 +1,16 @@
+package quickSort
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHeapSort(t *testing.T) {
+	//arr := []int{-1, 3, 4, 2, 0, 9}
+	arr := []int{3, 2, 8, 5, 9}
+	headSort(arr)
+	fmt.Println(arr)
+}
+func TestTopK(t *testing.T) {
+
+}
